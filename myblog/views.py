da09fad0c0e7f00391abdd868e19f167295f26db
Agasi Miroyan <limekick@mail.ru>
2024-11-16 03:48:14 +0400

    pagination completed

--- myblog/views.py
+++ myblog/views.py
@@ -6,8 +6,17 @@ from myblog.models import Post
 class HomeView(ListView):
     model = Post
     template_name = "myblog/index.html"
-    context_object_name = 'posts'
+    context_object_name = "posts"
     extra_context = {"title": "Главная страница"}
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page = context["page_obj"]
+        context["paginator_range"] = page.paginator.get_elided_page_range(
+            page.number, on_each_side=1, on_ends=1
+        )
+        return context
 
 
 class AboutView(TemplateView):
